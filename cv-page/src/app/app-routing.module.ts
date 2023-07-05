ecacdd241920c43f18db2f6be894f793855a880e
magda_m <magdamachacka@gmail.com>
2023-07-05 14:01:56 +0200

    routing works!

--- cv-page/src/app/app-routing.module.ts
+++ cv-page/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { WorksComponent } from './works/works.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about-component', component: AboutComponent },
+  { path: 'contact-component', component: ContactComponent },
+  { path: 'works-component', component: WorksComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
