bd89287136bd95b075059e4921622494268fc63a
magda_m <magdamachacka@gmail.com>
2023-07-10 12:44:43 +0200

    about component content and some service fixes

--- src/app/image-service.service.ts
+++ src/app/image-service.service.ts
@@ -9,27 +9,44 @@ export interface Image {
 }
 export interface Icon{
   iconPath: string,
-  link: string,
+  link: string | null,
+}
+export interface Social extends Icon{
+  iconLink:string | null;
 }
 export class ImageServiceService {
 
   private images: Image[] =
   [
     {
-      path: 'path',
-      title: 'title',
+      path: '/assets/pics/something.png',
+      title: 'CSS',
       description: 'description',
       link: 'link',
     }
   ];
 
   private icons: Icon[] = [
-    {
-      iconPath: 'path',
-      link: 'link',
-    }
-  ];
+    {iconPath: '/assets/pics/html-5.png',
+      link: 'https://www.flaticon.com/free-icons/html-5',},
+    {iconPath: '/assets/pics/css.png',
+      link: 'https://www.flaticon.com/free-icons/css',},
+      {iconPath: '/assets/pics/js.png',
+      link:"https://www.flaticon.com/free-icons/javascript",},
+      {iconPath: '/assets/pics/php.png',
+      link:'https://www.flaticon.com/free-icons/php',},
+      {iconPath: '/assets/pics/angular.png',
+      link:null,},
 
+  ];
+  private social: Social[] = [
+    {iconPath: '/assets/pics/github-sign.png',
+    link: 'https://github.com/mikemachacky',
+    iconLink: 'https://www.flaticon.com/free-icons/github' },
+    {iconPath: '/assets/pics/linkedin.png',
+    link: 'https://github.com/mikemachacky',
+    iconLink: 'https://www.flaticon.com/free-icons/linkedin' },
+  ];
   constructor() { }
 
   getImages(): Image[] {
@@ -37,5 +54,8 @@ export class ImageServiceService {
   }
   getIcons(): Icon[] {
     return this.icons;
+  }
+  getSocial(): Icon[] {
+    return this.social;
   }
 }
